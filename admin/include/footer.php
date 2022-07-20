1b44ae1cfd2c2a6fe104fd2a4a788243ad8946c0
EDWIN_NYONGESA <nyongesaedwin018@gmail.com>
2022-07-20 12:39:14 +0300

    initial commit

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+
+
+			<b class="copyright">&copy; copyright @BOOKLY <?php echo date('D-M-Y'); ?></b> All rights reserved :: Edwin Nyongesa
+		</div>
+	</div>
---
+++ includes/footer.php
@@ -0,0 +1,135 @@
+<footer id="footer" class="footer color-bg">
+    <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= CONTACT INFO ============================================================= -->
+                    <div class="contact-info">
+                        <div class="footer-logo">
+                            <div class="logo">
+                                <a href="index.php">
+
+                                    <h3>Shopping Portal</h3>
+                                    <!-- <li><a href="https://urbanstalls.com/sellerlogin/"><i class="icon fa fa-sign-in"></i>&nbsp Seller Login</a></li> -->
+                                    <li><a href=""><i class="icon fa fa-sign-in"></i>&nbsp Admin</a></li>
+                                    <li><a href=""></a>about us</li>
+                                    <li><a href=""></a>contacts</li>
+                                    <li><a href=""></a>return policy</li>
+                                    
+                                    <!-- <li><a href="https://urbanstalls.com/admin/courier/"><i class="icon fa fa-sign-in"></i>&nbsp Courier Service Login</a></li> 
+<li><a href="https://urbanstalls.com/sellerdetail.php"><i class="icon fa fa-sign-in"></i>&nbsp Feedback Seller</a></li> -->
+                                </a>
+                            </div><!-- /.logo -->
+
+                        </div><!-- /.footer-logo -->
+
+                        <div class="module-body m-t-20">
+                            <p class="about-us"> Brought To You By <a href="">edwin</a></p>
+                            <div class="social-icons">
+
+                                <a href="https://stackoverflow.com/users/19531531/nyongesa-edwin" target="_blank"><i class="icon fa fa-stack-overflow"></i></a>
+                                <a href="localhost/shopping/" class='active'><i class="icon fa fa-facebook"></i></a>
+                                <a href="https://github.com/arjan001" target="_blank" class='active'><i class="icon fa fa-github"></i></a>
+                                <a href="https://www.linkedin.com/in/edwin-nyongesa-770658230/" target="_blank"><i class="icon fa fa-linkedin"></i></a>
+                                <a href="https://www.instagram.com/BILGAX007" class='active' target="_blank"><i class="icon fa fa-instagram"></i></a>
+
+                            </div><!-- /.social-icons -->
+                        </div><!-- /.module-body -->
+
+                    </div><!-- /.contact-info -->
+                    <!-- ============================================================= CONTACT INFO : END ============================================================= -->
+                </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= CONTACT TIMING============================================================= -->
+                    <div class="contact-timing">
+                        <div class="module-heading">
+                            <h4 class="module-title">Delivery Times</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <tbody>
+                                        <tr>
+                                            <td>Monday-Friday:</td>
+                                            <td class="pull-right">10:00AM To 06:00PM</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Saturday:</td>
+                                            <td class="pull-right">11:00AM To 06:00PM</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Sunday:</td>
+                                            <td class="pull-right">12:00PM To 06:00PM</td>
+                                        </tr>
+                                        <tr>
+                                            <td>holidays</td>
+                                            <td class="pull-right">10:00PM To 06:00PM</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div><!-- /.table-responsive -->
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+                    <!-- ============================================================= CONTACT TIMING : END ============================================================= -->
+                </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= INFORMATION============================================================= -->
+                    <div class="contact-information">
+                        <div class="module-heading">
+                            <h4 class="module-title">information</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <ul class="toggle-footer">
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>KENYA</p>
+                                        <p>NAIROBI</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>(+254) 798325459<br>(+254) 113626187</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <span><a href="https://urbanstalls.com/">bookly.com</a></span>
+                                    </div>
+                                    <div class="media-body">
+                                        <span><b class="copyright">&copy; copyright @ <?php echo date('Y'); ?> All rights reserved:Edwin Nyongesa</b></span>
+
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+                    <!-- ============================================================= INFORMATION : END ============================================================= -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
